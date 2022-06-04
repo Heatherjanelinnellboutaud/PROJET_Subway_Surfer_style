a6324e46c4ea641acc48ef96b88beda671e765d3
Heather-Jane <heather-jane.linnell-boutaud@cpe.fr>
2022-06-04 20:10:13 +0200

    saut qui ne fonctionne pas

--- Projet opengl/projet/viewerGL.py
+++ Projet opengl/projet/viewerGL.py
@@ -5,6 +5,7 @@ import glfw
 import pyrr
 import numpy as np
 from cpe3d import Object3D
+import time
 
 class ViewerGL:
     def __init__(self):
@@ -64,6 +65,7 @@ class ViewerGL:
         self.cam = cam
 
     def update_camera(self, prog):
+        
         GL.glUseProgram(prog)
         # Récupère l'identifiant de la variable pour le programme courant
         loc = GL.glGetUniformLocation(prog, "translation_view")
@@ -115,8 +117,8 @@ class ViewerGL:
         if glfw.KEY_L in self.touch and self.touch[glfw.KEY_L] > 0:
             self.cam.transformation.rotation_euler[pyrr.euler.index().yaw] += 0.1
 
-        if glfw.KEY_SPACE in self.touch:
-            self.saut_montee()
+        if glfw.KEY_SPACE in self.touch and self.touch[glfw.KEY_SPACE] > 0:
+            self.saut()
                 
         
         self.cam.transformation.rotation_euler = self.objs[0].transformation.rotation_euler.copy() 
@@ -124,16 +126,23 @@ class ViewerGL:
         self.cam.transformation.rotation_center = self.objs[0].transformation.translation + self.objs[0].transformation.rotation_center
         self.cam.transformation.translation = self.objs[0].transformation.translation + pyrr.Vector3([0, 1, 5])
 
+    def saut(self):
+        i,j = 0,0
+        while i <= 5:
+            self.saut_montee()
+            i+=1
+        if i == 5:
+            while j <= 5:
+                self.saut_descente()
+                j+=1
+
+ 
+
+
     def saut_montee(self):
-        if self.objs[0].transformation.translation[1] <= 1.5:
-            self.objs[0].transformation.translation += \
-            pyrr.matrix33.apply_to_vector(pyrr.matrix33.create_from_eulers(self.objs[0].transformation.rotation_euler), pyrr.Vector3([0, 0.02, 0]))
-        else: 
-            self.saut_descente()
+        self.objs[0].transformation.translation += \
+        pyrr.matrix33.apply_to_vector(pyrr.matrix33.create_from_eulers(self.objs[0].transformation.rotation_euler), pyrr.Vector3([0, 0.02, 0]))
 
     def saut_descente(self):
-        if self.objs[0].transformation.translation[1] >= 1:
-            self.objs[0].transformation.translation -= \
-            pyrr.matrix33.apply_to_vector(pyrr.matrix33.create_from_eulers(self.objs[0].transformation.rotation_euler), pyrr.Vector3([0, 0.02, 0]))
-        else:
-            self.saut_descente()
+        self.objs[0].transformation.translation -= \
+        pyrr.matrix33.apply_to_vector(pyrr.matrix33.create_from_eulers(self.objs[0].transformation.rotation_euler), pyrr.Vector3([0, 0.02, 0]))
