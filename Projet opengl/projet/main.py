6da9ce94ca29f034fe726d88653c2e3a53ab0846
Heather-Jane <heather-jane.linnell-boutaud@cpe.fr>
2022-06-04 18:10:24 +0200

    v

--- Projet opengl/projet/main.py
+++ Projet opengl/projet/main.py
@@ -40,6 +40,19 @@ def main():
     o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
     viewer.add_object(o)
 
+# ROCHER
+    m = Mesh.load_obj('palmier.obj')    
+    m.normalize()
+    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 2, 2, 1]))
+    tr = Transformation3D()
+    tr.translation.y = -np.amin(m.vertices, axis=0)[1]
+    tr.translation.z = -5
+    tr.rotation_center.z = 0.2
+    texture = glutils.load_texture('palmier.jpg')
+    o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
+    viewer.add_object(o)
+
+
     m = Mesh()
     p0, p1, p2, p3 = [-25, 0, -25], [25, 0, -25], [25, 0, 25], [-25, 0, 25]
     n, c = [0, 1, 0], [1, 1, 1]
