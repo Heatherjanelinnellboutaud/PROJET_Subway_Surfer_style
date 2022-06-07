5125e5aa163779958e0cb350ec6c0ab4b8012638
Alexis Gueux <alexis.gueux@gmail.com>
2022-06-07 09:52:17 +0200

    classe

--- Projet opengl/projet/obstacle.py
+++ Projet opengl/projet/obstacle.py
@@ -8,27 +8,22 @@ import time
 class ObstacleGL:
     def __init__(self):
 
-       #obj = Object3D(image) 
-
        self.lst_obj = []
        self.numero_objet = 0
        self.vitesse = 0.2
        self.pox = 0
-       self.posy = 0
        self.posz = 25
 
-       
-    
     def add_object(self, obj):
         self.lst_obj.append(obj)
 
     def mvmt_obstacle(self,vitesse,numero_objet,objet):
         
         self.vitesse = vitesse
-        self.numero_objet = numero_objet
-        if self.objs[numero_objet-1].transformation.translation[2] >= -25:
-            self.objs[numero_objet-1].transformation.translation -= \
-            pyrr.matrix33.apply_to_vector(pyrr.matrix33.create_from_eulers(self.objs[0].transformation.rotation_euler), pyrr.Vector3([0, 0, vitesse]))
+        if self.lst_obj[numero_objet-1].transformation.translation[2] >= -25:
+            self.lst_obj[numero_objet-1].transformation.translation -= \
+            pyrr.matrix33.apply_to_vector(pyrr.matrix33.create_from_eulers(self.lst_obj[0].transformation.rotation_euler), pyrr.Vector3([0, 0, vitesse]))
+            print(self.lst_obj[numero_objet-1].transformation.translation)
         else:
             self.lst_obj.pop(objet)
 
