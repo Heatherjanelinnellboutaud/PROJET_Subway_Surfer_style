11d2b8e8d96ebd875793c1ab364d3e1f064d97ec
Alexis Gueux <alexis.gueux@gmail.com>
2022-06-07 08:28:38 +0200

    classe obst

--- Projet opengl/projet/obstacle.py
+++ Projet opengl/projet/obstacle.py
@@ -6,73 +6,27 @@ from cpe3d import Object3D
 import time
 
 class ObstacleGL:
-    def __init__(self,window):
+    def __init__(self):
 
-        # initialisation de la librairie GLFW
-        glfw.init()
-        # paramétrage du context OpenGL
-        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL.GL_TRUE)
-        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+       #obj = Object3D(image) 
 
-        self.window = window     
-        
-
-        self.objs = []
-
-    def run(self):
-        # boucle d'affichage
-        while not glfw.window_should_close(self.window):
-            
-
-            for obj in self.objs:
-                GL.glUseProgram(obj.program)
-                if isinstance(obj, Object3D):
-                    self.update_camera(obj.program)
-                obj.draw()
-
-            # changement de buffer d'affichage pour éviter un effet de scintillement
-            glfw.swap_buffers(self.window)
-            # gestion des évènements
-            glfw.poll_events()
-        
+       self.lst_obj = []
+       self.vitesse = 0.2
+       self.pox = 0
+       self.posy = 0
+       self.posz = 25
+    
     def add_object(self, obj):
         self.objs.append(obj)
 
-    def set_camera(self, cam):
-        self.cam = cam
-
-    def update_camera(self, prog):
+    def mvmt_obstacle(self,vitesse,numero_objet,objet):
         
-        GL.glUseProgram(prog)
-        # Récupère l'identifiant de la variable pour le programme courant
-        loc = GL.glGetUniformLocation(prog, "translation_view")
-        # Vérifie que la variable existe
-        if (loc == -1) :
-            print("Pas de variable uniforme : translation_view")
-        # Modifie la variable pour le programme courant
-        translation = -self.cam.transformation.translation
-        GL.glUniform4f(loc, translation.x, translation.y, translation.z, 0)
-
-        # Récupère l'identifiant de la variable pour le programme courant
-        loc = GL.glGetUniformLocation(prog, "rotation_center_view")
-        # Vérifie que la variable existe
-        if (loc == -1) :
-            print("Pas de variable uniforme : rotation_center_view")
-        # Modifie la variable pour le programme courant
-        rotation_center = self.cam.transformation.rotation_center
-        GL.glUniform4f(loc, rotation_center.x, rotation_center.y, rotation_center.z, 0)
-
-        rot = pyrr.matrix44.create_from_eulers(-self.cam.transformation.rotation_euler)
-        loc = GL.glGetUniformLocation(prog, "rotation_view")
-        if (loc == -1) :
-            print("Pas de variable uniforme : rotation_view")
-        GL.glUniformMatrix4fv(loc, 1, GL.GL_FALSE, rot)
-    
-        loc = GL.glGetUniformLocation(prog, "projection")
-        if (loc == -1) :
-            print("Pas de variable uniforme : projection")
-        GL.glUniformMatrix4fv(loc, 1, GL.GL_FALSE, self.cam.projection)
+        self.vitesse = vitesse
+        self.numero_objet = numero_objet
+        if self.objs[numero_objet-1].transformation.translation[2] >= -25:
+            self.objs[numero_objet-1].transformation.translation -= \
+            pyrr.matrix33.apply_to_vector(pyrr.matrix33.create_from_eulers(self.objs[0].transformation.rotation_euler), pyrr.Vector3([0, 0, vitesse]))
+        else:
+            self.lst_obj.pop(objet)
 
     
