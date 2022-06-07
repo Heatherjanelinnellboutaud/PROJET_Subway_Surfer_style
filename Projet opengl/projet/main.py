29ae7593c11ad55b1cccc03f8dce91a4deed2820
Heather-Jane <heather-jane.linnell-boutaud@cpe.fr>
2022-06-07 19:16:12 +0200

    f

--- Projet opengl/projet/main.py
+++ Projet opengl/projet/main.py
@@ -90,13 +90,6 @@ def main():
     o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, Transformation3D())
     viewer.add_object(o)
 
-    vao = Text.initalize_geometry()
-    texture = glutils.load_texture('fontB.jpg')
-    o = Text('', np.array([-0.8, 0.3], np.float32), np.array([0.8, 0.8], np.float32), vao, 2, programGUI_id, texture)
-    viewer.add_object(o)
-    o = Text('', np.array([-0.5, -0.2], np.float32), np.array([0.5, 0.3], np.float32), vao, 2, programGUI_id, texture)
-    viewer.add_object(o)
-
     viewer.run()
     
     
