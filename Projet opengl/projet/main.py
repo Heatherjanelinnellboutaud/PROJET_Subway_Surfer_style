5cc899ef2412ed3d374534bb0fa73beb2287614f
Heather-Jane <heather-jane.linnell-boutaud@cpe.fr>
2022-06-11 18:56:27 +0200

    tombe belle

--- Projet opengl/projet/main.py
+++ Projet opengl/projet/main.py
@@ -39,7 +39,7 @@ def main():
     tr.translation.y = -np.amin(m.vertices, axis=0)[1]
     tr.translation.z = -23
     tr.rotation_center.z = 0.2
-    texture = glutils.load_texture('tombe.jpg')
+    texture = glutils.load_texture('tombe.png')
     o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
     viewer.add_object(o)
 
